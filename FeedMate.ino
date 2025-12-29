4e6df97d68ed63af7222e8d5f163bdd486377c78
Liandri-Mining-Corp <julianbeck2003@gmail.com>
2025-12-29 10:24:26 +0100

    Added Getters from the other .ino files Added Constants to control the time to dispense the food Adjusted and Added new Inits for the other components.
    Adjusted the ESP Loop to check for the defined by the Constants, to check for the RFID and State of the Service Door, add the webServerLoop for the Webserver to function and to check the amount of the remaining food.

--- FeedMate.ino
+++ FeedMate.ino
@@ -1,18 +1,59 @@
+bool getServiceHatchState();
+float getHumidity();
+float getTemperature();
+
+const int FEEDING_TIME_HOUR = 14;
+const int FEEDING_TIME_MINUTE = 1;
+//const int FEEDING_TIME_SEC = 0; // Can be used to control to the Secound food shall be dispensed
+
 void setup(){
   //Enable Serial and set Baudrate
   Serial.begin(115200);
-  initRFIDHatch();
-  ultraAmpelInit();
-  moistureSensorinit();
-  webserverinit();
+  displayInit();
+  rfidInit();
+  serviceHatchInit();
+  ledControllerInit();
+  ultrasonicInit();
+  moistureSensorInit();
+  feederInit();
+  timerInit();
+  webServerInit();
+  displayBootDone(); 
 }
 
 void loop(){
-  webserverloop();
-  checkCard();
-  pulse();
-  changeLight();
-  CheckMoisture();
-  delay(1600);
+  if (isTime(FEEDING_TIME_HOUR, FEEDING_TIME_MINUTE, 0) == true)
+  {
+    turnfeeder();
+  }
+  
+  if (isAuthorizedCardDetected() == true && getServiceHatchState() == false)
+  {
+    Serial.println("opening");
+    openServiceHatch();
+    delay(1600);
+  } else if (isAuthorizedCardDetected() == true && getServiceHatchState() == true)
+  {
+    Serial.println("closing");
+    closeServiceHatch();
+    delay(1600);
+  }
+  webServerLoop();
+  updateLedByDistance(getDistance());
 }
 
+void turnfeeder() {
+  for (int i = 0; i < 4; i++) 
+  {
+    if (getFeederPOS() == 0)
+    {
+      setfeederPosition1();
+      delay(4000);
+    } else
+    {
+     setfeederPosition0();
+     delay(4000);
+    }
+  }
+  updateLedByDistance(getDistance());
+}
